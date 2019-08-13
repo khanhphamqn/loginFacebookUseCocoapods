bb6ac1c8412418a1c96ee1a7b8143e80e4ebde21
khanhphamqn <khanhphamqn@gmail.com>
2019-08-13 22:01:53 +0700

    finish code

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-
+        <Login/>
       </View>
     );
   }
